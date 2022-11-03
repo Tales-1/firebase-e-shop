eaf2fd4713e3fdf6c7be439da82a2d76f5be0da8
Tales-1 <j.naz248@outlook.com>
2022-11-03 18:08:19 +0000

    Added authentication and dynamically rendered page depending on user status

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -1,10 +1,46 @@
 
-const Dashboard:React.FC = () => {
+import { useEffect, useState } from "react";
+import { useAuthState } from "react-firebase-hooks/auth";
+import { useNavigate } from "react-router-dom";
+import { auth, db, logout } from "../firebase";
+import { query, collection, getDocs, where } from "firebase/firestore";
+import { doc, setDoc, addDoc } from "firebase/firestore";
 
+import Button from "../components/Button";
+const Dashboard:React.FC = () => {
+    const [user, loading, error] = useAuthState(auth);
+    const [name, setName] = useState("");
+    const navigate = useNavigate();
+
+    const fetchUserName = async () => {
+        try {
+            const q = query(collection(db,"users"), where("uid","==",user?.uid))
+            const doc = await getDocs(q)
+            const data = doc.docs[0].data()
+            setName(data.name)
+         } catch (err) {
+            console.error(err)
+            alert("An error occured while fetching user data");
+        }
+    }
+
+    useEffect(()=>{
+        if(loading) return
+        if(!user) return navigate("/profile/login")
+        fetchUserName()
+        const prodRef = collection(db,"products/test/children")
+        addDoc(prodRef,{id:1})
+
+    },[user,loading])
+
+   
+    
+    
     return (
         <div className="w-screen h-screen grid items-center">
-            <h1>Welcome User!</h1>
-
+            <h1>Welcome {name}!</h1>
+            <div>email: {user?.email}</div>
+            <Button styles="p-2" func={logout}>Logout</Button>
         </div>
     )
 }
