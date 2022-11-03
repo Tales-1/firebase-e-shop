2809b3975f19d0609f4392aaef0175663bdfe560
Tales-1 <j.naz248@outlook.com>
2022-11-03 18:12:04 +0000

    Reconfigured 'to' attribute in Link tag

--- src/components/header/header-items/Profile.tsx
+++ src/components/header/header-items/Profile.tsx
@@ -3,7 +3,7 @@ import {Link} from "react-router-dom"
 
 const Profile:React.FC = () => { 
     return (
-        <Link to="/user-profile"><img 
+        <Link to="/profile"><img 
             src={profileIcon} 
             className="w-8 mr-3 xl:w-12 xl:mr-5 hover:cursor-pointer"
             alt="profile icon"
