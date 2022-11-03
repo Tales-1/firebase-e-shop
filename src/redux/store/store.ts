2c57a9bb571ed5ad14e68013afcca528b905c728
Tales-1 <j.naz248@outlook.com>
2022-11-03 18:19:45 +0000

    Added redux persist to the app

--- src/redux/store/store.ts
+++ src/redux/store/store.ts
@@ -1,15 +1,44 @@
-import { configureStore, ThunkAction, Action } from '@reduxjs/toolkit';
+import { configureStore, ThunkAction, Action, combineReducers } from '@reduxjs/toolkit';
 import dataReducer from "../features/dataSlice"
 import screenReducer from "../features/screenSlice"
 import cartReducer from "../features/cartSlice"
+import { persistReducer, persistStore,FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER, } from "redux-persist"
+import storage from "redux-persist/lib/storage"
+import thunk from "redux-thunk"
+
+
+const persistConfig = {
+  key:"root",
+  storage,
+  blacklist:["screen"]
+}
+
+const reducers = combineReducers(
+    {
+      fetcher:dataReducer,
+      cart:cartReducer,
+      screen:screenReducer
+  })
+
+const persistedReducers = persistReducer(persistConfig,reducers)
 
 export const store = configureStore({
-  reducer: {
-    fetcher:dataReducer,
-    screen:screenReducer,
-    cart:cartReducer
-  },
+  reducer:persistedReducers,
+  middleware: (getDefaultMiddleware) =>
+        getDefaultMiddleware({
+            serializableCheck: {
+                ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+            },
+        }),
+  devTools: process.env.NODE_ENV !== 'production',
 });
+
+export const persister = persistStore(store)
 
 export type AppDispatch = typeof store.dispatch;
 export type RootState = ReturnType<typeof store.getState>;
