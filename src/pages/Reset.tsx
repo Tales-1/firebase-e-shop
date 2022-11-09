dd8c65cb26981f3ffd287bec22cca2b4778797ff
Tales-1 <j.naz248@outlook.com>
2022-11-09 16:48:23 +0000

    Changed border radius on elements

--- src/pages/Reset.tsx
+++ src/pages/Reset.tsx
@@ -18,13 +18,13 @@ const Reset:React.FC = () => {
       
     return(
         <div className="flex justify-center h-screen bg-dark md:items-center">
-            <div className="bg-white p-6 max-w-md w-full h-fit grid items-center shadow-md">
+            <div className="bg-white p-6 max-w-md w-full h-fit grid items-center shadow-md rounded-md">
                 <div className="flex flex-col gap-4 w-11/12 mx-auto">
                 <h1 className="text-start font-sans-serif text-xl">Forgot Password</h1>
                     <form className="flex flex-col gap-5">
                         <input
                             type="text"
-                            className="border-2 border-black p-2"
+                            className="border-2 border-black p-2 rounded-md"
                             value={email}
                             onChange={(e) => setEmail(e.target.value)}
                             placeholder="E-mail Address"
@@ -32,13 +32,13 @@ const Reset:React.FC = () => {
                     </form>
                 <div className="text-center mt-3">
                     <button
-                        className="p-3 bg-blue-card font-bold text-white"
+                        className="p-3 bg-blue-card font-bold text-white rounded-md"
                         onClick={() => sendPasswordReset(email)}>
                         Send password reset email
                      </button>
                 </div>
                 <div className="text-center">
-                    Don't have an account? <Link to={`/${path}/register`}>Register</Link> now.
+                <Link to={`/${path}/register`} className="hover:opacity-80 underline"> Don't have an account? Register now.</Link>
                 </div>
             </div>
       </div>
