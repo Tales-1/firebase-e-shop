9b1431dcd7535098c8b1f83a305c226f5c110897
Tales-1 <j.naz248@outlook.com>
2022-11-09 16:50:00 +0000

    Added reducers to show notifications and cartMenu

--- src/redux/features/screenSlice.ts
+++ src/redux/features/screenSlice.ts
@@ -9,6 +9,12 @@ interface State {
         desktop:boolean
     }
     styles:string
+    translate:string
+    translateCart:string
+    msg:string
+    overlayHidden:boolean
+    // type:string
+
 }
  
 const initialState:State = {
@@ -17,7 +23,12 @@ const initialState:State = {
         tablet:window.innerWidth > 768 && window.innerWidth < 1030,
         desktop:window.innerWidth > 1030
     },
-    styles:"before:absolute before:inset-0 before:bg-black before:opacity-40"
+    styles:"before:absolute before:inset-0 before:opacity-30",
+    translate:"-translate-y-full",
+    translateCart:"translate-x-full",
+    msg:"empty",
+    overlayHidden:true
+    // type:"",
 }
 
 export const screenSlice = createSlice({
@@ -28,13 +39,34 @@ export const screenSlice = createSlice({
             state.viewportSizes.mobile = action.payload < 768
             state.viewportSizes.tablet = action.payload > 768 && action.payload < 1030,
             state.viewportSizes.desktop = action.payload > 1030
-        }
+        },
+        showNotification:(state, action) => {
+            // state.type = action.payload.type
+            state.msg = action.payload
+            state.translate = "translate-y-0"
+        },
+        hideNotification:(state) => {
+            state.translate ="-translate-y-full"
+        },
+        showCart:(state) => {
+            state.translateCart = "translate-x-0"
+            state.overlayHidden = false
+        }, 
+        hideCart:(state) => {
+            state.translateCart = "translate-x-full"
+            state.overlayHidden = true
+        },
     }
 })
 
-export const { setWidth } = screenSlice.actions
+export const { setWidth, showNotification, hideNotification, hideCart, showCart } = screenSlice.actions
 
 export const selectViewport = (state:RootState) => state.screen.viewportSizes
+export const selectTranslate = (state:RootState) => state.screen.translate
 export const selectOverlay = (state:RootState) => state.screen.styles
+export const selectMsg = (state:RootState) => state.screen.msg
+export const selectTranslateCart = (state:RootState) => state.screen.translateCart
+export const selectOverlayHidden = (state:RootState) => state.screen.overlayHidden
+// export const selectType = (state:RootState) => state.screen.type
 
 export default screenSlice.reducer
