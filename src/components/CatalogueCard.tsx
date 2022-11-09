c479f374c8d3132707143adb06852bc1320ff201
Tales-1 <j.naz248@outlook.com>
2022-11-09 16:35:41 +0000

    Changed width on tablet viewport and adjusted border radius

--- src/components/CatalogueCard.tsx
+++ src/components/CatalogueCard.tsx
@@ -15,16 +15,22 @@ type CardProps = {
 
 const CatalogueCard:React.FC<CardProps> = ({url,src,name}) => { 
     const overlayStyles = useAppSelector(selectOverlay)
+    let nameArray = name.split("")
+    let displayName = nameArray.map((letter)=>{
+        return <span className="block text-center text-3xl lg-2:text-4xl">{letter.toUpperCase()}</span>
+    })
     return( 
-                <div className={`relative ${overlayStyles} flex flex-col w-full items-center shadow-2xl transition-all duration-300 hover:scale-105 rounded-3xl overflow-hidden`}>
-                    <img src={src} alt="clothes" className="w-full aspect-square object-cover"/>
-                    <div className="absolute inset-0 flex flex-col gap-14 justify-center items-center">
-                        <h2 className="text-5xl lg-2:text-6xl text-white">{name}</h2>
-                        <Link to={url}>
-                            <Button styles="text-white text-lg md:text-3xl lg:text-xl lg-2:text-2xl lg:border-4 border-white border-2 p-2 hover:bg-white hover:text-black">SHOP NOW</Button>
-                        </Link>
+                <Link to={url} className="w-full md:w-[26rem] lg-2:w-full">
+                    <div className="relative bg-blue-header flex flex-col w-full items-center shadow-2xl transition-all duration-300 hover:scale-105 rounded-2xl overflow-hidden">
+                        <div className={`relative ${overlayStyles} before:bg-black w-full ml-auto`}>
+                            <img src={src} alt="clothes" className={`aspect-square object-cover ml-auto w-full`}/>
+                        </div>
+                        <div className="flex flex-col absolute justify-center items-center h-full w-full">
+                            
+                            <h2 className="text-white text-5xl font-serif tracking-wider lg-2:text-6xl">{name}</h2>
+                        </div>
                     </div>
-                </div>
+                </Link>
     )
 }
 
