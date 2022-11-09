a941bc59bc2c27e9aa934c9ec38bc1f25874948a
Tales-1 <j.naz248@outlook.com>
2022-11-09 16:35:02 +0000

    added default styling to Button component

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,14 +1,17 @@
 type ButtonProps = { 
-    children:string,
+    children:string | JSX.Element,
     styles:string,
     func?:() => void
+    id?:string
 }
 
 
-const Button:React.FC<ButtonProps> = ({children,styles,func}) => { 
+const Button:React.FC<ButtonProps> = ({children,styles,func,id}) => { 
 
     return(
-        <button className={styles} onClick={func}>{children}</button>
+        <button id={id} className={`${styles} rounded-md transition-all duration-100 hover:scale-105 focus:ring focus:outline-none focus:ring-blue-card`} onClick={func}>
+            {children}
+        </button>
     )
 }
 
