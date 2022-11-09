ddeb0f0bd71b1b45233b0cfe86fd5212eb5cbfd5
Tales-1 <j.naz248@outlook.com>
2022-11-09 16:45:38 +0000

    Adjusted viewport height

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -4,9 +4,9 @@ import ImageGrid from "../components/ImageGrid"
 const Home:React.FC = () =>{
     
     return(
-        <div className="h-full">
-            
+        <div className="h-[89vh] lg-2:h-[82vh]">
            <ImageGrid />
+
         </div>
     )
 }
