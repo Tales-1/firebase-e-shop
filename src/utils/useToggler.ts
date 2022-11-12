3132518e8496bf6cb290aea468820ecc2ef4abba
Tales-1 <j.naz248@outlook.com>
2022-11-12 12:46:19 +0000

    added a toggle function for hamburger menu

---
+++ src/utils/useToggler.ts
@@ -0,0 +1,22 @@
+import { useAppDispatch, useAppSelector } from "../redux/store/hooks";
+import { selectHamburgerVisible, toggleHamburger, selectViewport } from "../redux/features/screenSlice";
+import React, { ReactElement } from "react";
+
+
+// WHAT TYPE DO I GIVE TO THIS HOOK?
+const useToggler = () => {
+    const viewport = useAppSelector(selectViewport)
+    const {mobile} = viewport
+    const dispatch = useAppDispatch()
+    const isVisible = useAppSelector(selectHamburgerVisible)
+    function toggleMenu(defaultValue:string){
+        if(mobile){
+            dispatch(toggleHamburger(defaultValue))
+        }
+    }
+    
+    return {toggleMenu, isVisible}
+}
+
+
+export default useToggler
