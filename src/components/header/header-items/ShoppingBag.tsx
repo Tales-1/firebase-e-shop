cf64c666d02e436f07fc4856f53852bf3952797c
Tales-1 <j.naz248@outlook.com>
2022-11-12 12:35:15 +0000

    Refactored

--- src/components/header/header-items/ShoppingBag.tsx
+++ src/components/header/header-items/ShoppingBag.tsx
@@ -12,7 +12,7 @@ const ShoppingBag: React.FC = () => {
      const shoppingBag = !isEmpty ? emptyShoppingBag : fullShoppingBag
 
     return(
-        <Link to="/checkout">
+        <Link to="/cart">
             <img 
             src={shoppingBag} 
             className="w-7 align-middle hover:cursor-pointer md:w-7 xl:w-10"
