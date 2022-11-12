458ff1a407830169cab03ba0a4b97e8e3745e193
Tales-1 <j.naz248@outlook.com>
2022-11-12 12:32:57 +0000

    Imported toggle function from redux store

--- src/components/header/hamburger/MobileNavBar.tsx
+++ src/components/header/hamburger/MobileNavBar.tsx
@@ -1,18 +1,19 @@
-import {useState} from "react"
+import { useState } from "react"
 import HamburgerMenu from "./HamburgerMenu"
 import SlidingMenu from "./SlidingMenu"
-
+import useToggler from "../../../utils/useToggler"
 
 
 const HamburgerNav:React.FC = () => { 
-    const [visible,setVisible] = useState(false)
-    const translateClass = visible ? "translate-x-0" : "-translate-x-full"
-
+    const {isVisible, toggleMenu} = useToggler()
     
+    const translateClass = isVisible ? "translate-x-0" : "-translate-x-full"
+
+  
     return(
         <div className="pl-4 z-20">
-            <HamburgerMenu toggleVisible={setVisible} />
-            <SlidingMenu translate={translateClass} setVisible={setVisible} />
+            <HamburgerMenu toggleVisible={()=>toggleMenu("OPEN")} />
+            <SlidingMenu translate={translateClass} toggle={()=>toggleMenu("CLOSE")} />
         </div>
     )
 }
