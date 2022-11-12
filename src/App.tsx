a0bbbee0272ddf2c6495ecf437b1a1a300845f59
Tales-1 <j.naz248@outlook.com>
2022-11-12 12:42:07 +0000

    Reorganised structure of routes and moved fetch function to App component

--- src/App.tsx
+++ src/App.tsx
@@ -1,45 +1,72 @@
 import {BrowserRouter as Router, Route,Routes,Navigate} from "react-router-dom"
-import React from "react"
+import React, { useEffect } from "react"
 import Home from "./pages/Home";
 import Catalogue from "./pages/Catalogue";
 import ProductList from "./pages/ProductList";
-import ContactPage from "./pages/ContactPage";
+import Checkout from "./pages/cart/checkout/Checkout";
+import Contact from "./pages/Contact";
 import ProductPage from "./pages/ProductPage";
-import Login from "./pages/Login";
-import Checkout from "./pages/Checkout";
-import Header from "./components/header/Header";
-import Dashboard from "./pages/Dashboard";
-import Register from "./pages/Register";
-import Reset from "./pages/Reset"
+import Login from "./pages/profile/Login";
+import Cart from "./pages/cart/Cart";
+import Dashboard from "./pages/profile/dashboard/Dashboard";
+import Register from "./pages/profile/Register";
+import Reset from "./pages/profile/Reset"
 import Notification from "./components/Notification";
-import SlidingCartMenu from "./components/SlidingCartMenu";
 import Overlay from "./utils/Overlay";
+import Buffer from "./pages/Buffer";
+import Orderhistory from "./pages/profile/dashboard/dashboard-routes/Orderhistory";
+import Wishlist from "./pages/profile/dashboard/dashboard-routes/Wishlist";
+import AccounDetails from "./pages/profile/dashboard/dashboard-routes/AccountDetails";
+import { selectStatus, fetchData, selectProducts } from "./redux/features/dataSlice";
+import { useAppDispatch, useAppSelector } from "./redux/store/hooks";
+import Delivery from "./pages/cart/checkout/Delivery";
+import Information from "./pages/cart/checkout/Information";
+import Payment from "./pages/cart/checkout/Payment";
+import Review from "./pages/cart/checkout/Review";
 
 
 const App: React.FC = () => {
+  const dispatch = useAppDispatch()
+  const status = useAppSelector(selectStatus)
+  useEffect(()=>{
+    if(status === "idle"){
+      dispatch(fetchData())
+  }
+  },[status,dispatch])
+  console.log(useAppSelector(selectProducts))
   return (
     <div className="relative flex flex-col h-screen">
       <Notification />
       <Overlay />
       <Router>
-        <Header />
-        <SlidingCartMenu />
         <Routes>
-            <Route path ="/" element={<Home />} />
-            <Route path="/collection" element={<Catalogue />} />
-            <Route path="/collection/:name">
-              <Route index element={<ProductList />} />
-              <Route path=":type" element={<ProductPage />} />
+            <Route path ="/" element={<Home />}>
+              <Route path="collection" element={<Catalogue />} />
+              <Route path="collection/:name">
+                  <Route index element={<ProductList />} />
+                  <Route path=":type" element={<ProductPage />} />
+              </Route>
+              <Route path="contact-us" element={<Contact />} />
+              <Route path="cart" element={<Cart />} />
+              <Route path="profile">
+                  <Route index element={<Buffer />} />
+                  <Route path ="login" element={<Login />} />
+                  <Route path ="register" element={<Register />} />
+                  <Route path ="reset" element={<Reset />} />
+                  <Route path="dashboard" element={<Dashboard />}>
+                      <Route path="order-history" element={<Orderhistory />}/>
+                      <Route path="wishlist" element={<Wishlist />}/>
+                      <Route path="account-details" element={<AccounDetails />}/>
+                  </Route>
+              </Route>
             </Route>
-            <Route path="/contact-us" element={<ContactPage />} />
-            <Route path="/checkout" element={<Checkout />} />
-            <Route path="/profile">
-              <Route index element={<Dashboard />} />
-              <Route path="login" element={<Login />} />
-              <Route path ="register" element={<Register />} />
-              <Route path ="reset" element={<Reset />} />
+            <Route path="/checkout" element={<Checkout />}>
+              <Route path="information" element={<Information />} />
+              <Route path="delivery" element={<Delivery />}/>
+              <Route path="payment" element={<Payment />}/>
+              <Route path="review" element={<Review />}/>
             </Route>
-            <Route path="/dashboard" element={<Dashboard />} />
+            
             <Route path="*" element={<Navigate to="/" replace />} />
         </Routes>
       </Router>
