bd203ca94d13e71b45f3eab82529929e6efb57ca
Tales-1 <j.naz248@outlook.com>
2022-11-12 12:38:13 +0000

    Added underline to the title on product list page

--- src/pages/ProductList.tsx
+++ src/pages/ProductList.tsx
@@ -5,7 +5,7 @@ import SpinnerL from "../utils/SpinnerL"
 import BreadCrumbs from "../utils/BreadCrumbs"
 import { selectError, selectStatus, selectFiltered, fetchData, filter} from "../redux/features/dataSlice"
 import { useAppDispatch, useAppSelector } from "../redux/store/hooks"
-
+import Underline from "../images/PngItem_1128059.png"
 interface ProductObject { 
     id?:string,
     name?:string,
@@ -22,9 +22,7 @@ const ProductList:React.FC = () =>{
     const dispatch = useAppDispatch()
     
     useEffect(()=>{
-        if(status === "idle"){
-            dispatch(fetchData())
-        } else if(status === "fulfilled"){
+        if(status === "fulfilled"){
             dispatch(filter(params))
         }
     },[status,dispatch,params])
@@ -33,6 +31,7 @@ const ProductList:React.FC = () =>{
     
     const displayItems:ReactNode = filteredArray.map((product:ProductObject,i)=>{
             const { name,price,id,url } = product
+        
             return (<ProductCard 
                         key={i}
                         name={name}
@@ -42,14 +41,17 @@ const ProductList:React.FC = () =>{
                     />)
         
     })
-
+   
     return(
         <div className="container h-screen min-w-full">
             <BreadCrumbs pathObj={{path:pathname, name:params}} />
-            <h2 className="text-4xl py-6 w-full text-center md:text-5xl font-serif">{params?.toUpperCase()}</h2>
+            <div className="mt-8 w-full grid grid-cols-3 grid-rows-2 items-center justify-center">
+                <h1 className="text-4.5xl lg:text-5xl font-serif text-center row-start-1 col-start-1 col-span-3">{params?.toUpperCase()}</h1>
+                <img src={Underline} alt="underline" className="w-2/5 min-w-[10rem] max-w-[14rem] row-start-2 col-start-1 col-span-3 mx-auto"/>
+            </div>
+            
             <main className="flex flex-col lg:flex-row">
-                
-                <div className="bg-white mt-5 flex flex-col gap-10 justify-center items-center py-5 md:w-10/12 md:mx-auto md:flex-row md:flex-wrap lg:items-start lg-2:w-8/12  lg:ml-auto lg:mr-20">
+                <div className="bg-white mt-5 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 items-stretch mx-auto justify-items-center md:flex-row md:flex-wrap gap-10 py-5 md:w-10/12 lg-2:w-8/12  lg:ml-auto lg:mr-20">
                     {status === "loading" ? <SpinnerL /> :  displayItems }    
                 </div>
             </main>
@@ -58,4 +60,4 @@ const ProductList:React.FC = () =>{
     )
 }
 
-export default ProductList
+export default ProductList          
