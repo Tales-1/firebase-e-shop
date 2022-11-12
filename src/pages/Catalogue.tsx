15b994df1aa0cd7e53bf941c8348d513bb3f8a33
Tales-1 <j.naz248@outlook.com>
2022-11-12 12:36:05 +0000

    Added underline underneath catalogue title

--- src/pages/Catalogue.tsx
+++ src/pages/Catalogue.tsx
@@ -2,13 +2,16 @@ import kurtaOne from "../images/Kurta-1.jpeg"
 import kurtaTwo from "../images/Kurta-2-0.jpeg"
 import kurtaThree from "../images/Kurta-3.jpeg"
 import CatalogueCard from "../components/CatalogueCard"
-
+import Underline from "../images/PngItem_1128059.png"
 
 const Catalogue:React.FC = () =>{
     
     return(
         <div className="flex flex-col items-center h-full gap-6">
-            <h1 className="text-4.5xl lg:text-6xl font-serif mt-12 w-full text-center">COLLECTION</h1>
+            <div className="mt-16 w-full grid grid-cols-3 grid-rows-2 items-center justify-center">
+                <h1 className="text-4.5xl lg:text-6xl font-serif text-center row-start-1 col-start-1 col-span-3">COLLECTION</h1>
+                <img src={Underline} alt="underline" className="w-2/3 min-w-[12rem] max-w-[23rem] row-start-2 col-start-1 col-span-3 mx-auto"/>
+            </div>
             <div className="flex flex-col lg-2:flex-row justify-center items-center gap-8 bg-white w-full sm:w-9/12 lg:w-10/12 p-10">
                 <CatalogueCard url="/collection/kurtas" src={kurtaOne} name="Kurtas" />
                 <CatalogueCard url="/collection/dresses" src={kurtaTwo} name="Dresses" />
