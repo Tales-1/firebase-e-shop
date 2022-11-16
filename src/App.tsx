9a5ea88622d046b5b474c0a720720fe10f3d86dc
Tales-1 <j.naz248@outlook.com>
2022-11-16 12:24:35 +0000

    Changed isVisible value to reflect changes in the loading state of the component

--- src/App.tsx
+++ src/App.tsx
@@ -1,39 +1,37 @@
 import {BrowserRouter as Router, Route,Routes,Navigate} from "react-router-dom"
-import React, { useEffect, useState } from "react"
+import React, { useEffect, useLayoutEffect } from "react"
 import AnimatedRoutes from "components/AnimatedRoutes";
 // UTLS & MISC
 import Notification from "./components/misc/Notification";
 import Overlay from "./utils/Overlay";
 import useFetcher from "./utils/hooks/useFetcher";
 import useStoreUser from "./utils/hooks/useStoreUser";
-import { AnimatePresence } from "framer-motion";
 import Welcome from "pages/Welcome_PG";
+import { selectLoading } from "redux/features/screenSlice";
+import { useAppSelector } from "redux/store/hooks";
 
 const App: React.FC = () => {
   const {status, fetch, dispatch} = useFetcher()
   const {isLoggedIn} = useStoreUser()
-  const [isVisible,setVisible] = useState(true)
+  const isVisible = useAppSelector(selectLoading)
+
   useEffect(()=>{
     if(status === "loading"){
       fetch()
+      console.log(("fetched"))
     }
-    setTimeout(()=>setVisible(false),1500)
   },[status,dispatch])
+  
 
- 
   return (
     <div className="relative flex flex-col h-fit">
-      
-    <Welcome isVisible={isVisible}/>
-    
+      <Welcome isVisible={isVisible}/>
       <Notification />
       <Overlay />
-      <AnimatePresence exitBeforeEnter>
         <Router>
           <AnimatedRoutes isLoggedIn={isLoggedIn} />
 
         </Router>
-      </AnimatePresence>
       
     </div>
   );
