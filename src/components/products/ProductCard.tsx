64547b670d12078fe2dc3a47ac1f8968900bdc99
Tales-1 <j.naz248@outlook.com>
2022-11-17 17:21:38 +0000

    Styled product cards to be smaller on mobile

--- src/components/products/ProductCard.tsx
+++ src/components/products/ProductCard.tsx
@@ -21,11 +21,12 @@ const ProductCard:React.FC<Props> = ({name,price,urls,id}) => {
             initial={{opacity:0,}}
             animate={{opacity:1,transition:{duration:.5, delay:0.6, ease:"easeInOut"}}}
             whileHover={{scale:1.1}}
-            className="min-h-card w-full max-w-[17rem] rounded-lg flex flex-col items-center will-change-transform
-            ">
+            className="flex flex-col items-center 
+                        min-h-card w-full max-w-[12rem] md:max-w-[14rem] lg-2:max-w-[17rem] rounded-lg will-change-transform">
                 
             <div className="grid relative shadow-lg h-full w-full">
-                <Link to={`${pathname}/${name?.split(" ").join("-")}`} className="row-start-1 col-start-1" onClick={()=>dispatch(findCurrent(id))}>
+                <Link to={`${pathname}/${name?.split(" ").join("-")}`} className="row-start-1 col-start-1" 
+                        onClick={()=>dispatch(findCurrent(id))}>
                     <LazyLoadImage
                         className="rounded-md max-h-card h-full w-full object-cover" 
                         src={urls?.[0]}
@@ -38,7 +39,7 @@ const ProductCard:React.FC<Props> = ({name,price,urls,id}) => {
                     <p className="row-start-1 col-start-1 mt-8 ml-4 rotate-[41deg] text-white font-bold">£{price}</p>
                 </div>
             </div>
-            <h3 className="text-lg md:text-xl font-bold font-serif">{name}</h3>
+            <h3 className="text-lg md:text-xl font-bold font-serif text-center">{name}</h3>
             
         </motion.article>
     )
