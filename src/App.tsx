19a39a593b999877f4fa0974f9b6b0973dd18499
Tales-1 <j.naz248@outlook.com>
2022-11-18 10:04:11 +0000

    Switched browser router to hashrouter to allow gh pages to access routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router } from "react-router-dom"
+import { HashRouter as Router } from "react-router-dom"
 import React, { useEffect } from "react"
 import AnimatedRoutes from "components/AnimatedRoutes";
 // UTLS & MISC
